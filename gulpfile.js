43bd7a4bdb5e41a1f9d68ebfebdc7e6e97f39577
Samvel <32421202+HayrapetyanStyle@users.noreply.github.com>
2020-01-30 16:40:45 +0400

    new update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,8 @@
-/* 
-	GULP-ով աշխաելու համար նախ պետք է ներբեռնեք Nodejs.org կայքից nodejs-ը, որի մեջ արդեն իսկ կա NPM-ը որը NodeJs Packed Manager է
-	բացվում, այսինքն նոդ ջիեսի պակետների մենեջեր, հետո նստացվնում եք գալպը CMDի միջոցով գրելով npm i gulp-cli --save-dev
-	հետո սարքում ենք ձեր json-ը գրելով nmp init ու լրացնելով դաշտերը, եթե ցանկանում եք բաց թողնել պարզապես գրեք npm init հետո y
-	ու վերջ գալպը պատրաստ է աշխատանքի, հետո ձեռքով նստացնում եք բոլոր մոդուլները, որոնք ներքևում են, և վերջ, խոդի եք տալիս պրոյեկտը ըստ ֆունկցիաների
-*/
+/* SAMi GULP 4 File 2019 v3 */
 
+// update [imagemin] up to 7
 
-
-// Ստանում ենք գալպի այն մոդուլները որոնց մեզ պետք են
+// get npm modules
 
 const gulp = require('gulp');
 const concat = require('gulp-concat');
@@ -21,10 +16,10 @@ const imagemin = require('gulp-imagemin');
 const gcmq = require('gulp-group-css-media-queries');
 const sourcemaps = require('gulp-sourcemaps');
 
-// ցույց ենք տալիս այն ճանապարհը, որտեղ որ պետք է build լինի մեր վերջնական պրոյեկտը
+// build path
 const patt = './build';
 
-// sass-ի ֆունկցիան
+// sass to css task
 function preproc() {
    return gulp.src('./src/sass/styles.scss')
    .pipe(sass().on('error', sass.logError))
@@ -42,7 +37,8 @@ function preproc() {
    .pipe(gulp.dest('./build/css'))
    .pipe(browserSync.stream());
 }
-// javascrip-ների ֆունկցիան
+
+// styles task
 function styles() {
    return gulp.src('./src/css/**/*.css')
    .pipe(gcmq())
@@ -58,7 +54,8 @@ function styles() {
    .pipe(gulp.dest('./build/css'))
    .pipe(browserSync.stream());
 }
-// all js
+
+// all js task
 function scripts() {
    return gulp.src('./src/js/**/*.js')
    .pipe(sourcemaps.init())
@@ -70,13 +67,14 @@ function scripts() {
    .pipe(gulp.dest('./build/js'))
    .pipe(browserSync.stream());
 }
-// imagemin-ի ֆունկցիան, որը նկարները մինիֆիկացնում է
+
+// pictures task
 function img(){
-	return gulp.src('./src/img/*')
+	return gulp.src('./src/img/**/*')
 	.pipe(imagemin())
 	.pipe(imagemin([
 		imagemin.gifsicle({interlaced: true}),
-		imagemin.jpegtran({progressive: true}),
+		imagemin.mozjpeg({quality: 75, progressive: true}),
 		imagemin.optipng({optimizationLevel: 5}),
 		imagemin.svgo({
 			plugins: [
@@ -84,49 +82,54 @@ function img(){
 				{cleanupIDs: false}
 			]
 		})
-   ]))
+	]))
    
    .pipe(gulp.dest('./build/img'))
 }
-// del-ը ջնջելու ֆունկցիան է, որը ջնջում է կոնկրետ ցույց տված ֆայլը, եթե դիրքեթիվը փոխեք ուշադիր եղեք, որ ճիշտ նշեք նոր դիրեքթիվը,
-// թե չէ շատ հնարավոր ա ձեր կոմպի մեջ ինչ կա չկա պահի տակ ջնջեք :) 
+
+// delete build folder and all files in the build folder
 function clean() {
    return del(['build/*'])
 }
-// fonts սա ֆոնթերի ֆունկցիան է, որը ֆոնթերը src-ից քոփի է անում և տեղադրում build-ի մեջ
+
+// fonts folder => build
 function fonts(){
    return gulp.src('./src/fonts/*')
    .pipe(gulp.dest('./build/fonts'))
 }
-// libs սա այն ֆունկցիան է, որտեղ կարոն եք դնել բոլոր գրադարանները, կամ պլագինները, որոնք առանձին են միանում մեր HTMLին,
-// օրինակ ինչ որ image gallery-ի պլագին, որը միայն պետք է աշխատի gallery էջում:
+
+// all libs folder => build
 function libs(){
    return gulp.src('./src/libs/*')
    .pipe(gulp.dest('./build/libs')) 
 }
-// html այս ֆունկցիան բոլոր HTML ները տեղափոխում է src-ից դեպի build folder որտեղ էլ ստանում ենք վերջնական ֆայլերը:
+
+// all .html => build
 function htmls(){
    return gulp.src('./src/*.html')
    .pipe(gulp.dest('./build/')) 
    .pipe(browserSync.stream());
 }
 
-// gulp watch - սա այն հրամանն է որի միջոցով մենք ապահովում ենք ուղիղ եթեր, և որը վերահսկում է բոլոր փոփոխությունները
-// կլինի sass, less, styleus, css, js, html և այլն, ներկա պահին 3 տասկ է կատարում
+// run gulp watch to live stream in browser
 function watch() {
-   browserSync.init({
-      server: {
-          baseDir: patt
-      }
-  });
+	browserSync.init({
+	  server: {
+		 baseDir: patt
+	  }//,
+	  //tunnel: true
+	});
 
-gulp.watch('./src/sass/**/*.scss', preproc)// sass ների թասք
-  gulp.watch('./src/css/**/*.css', styles)// css ների թասք
-  gulp.watch('./src/*.html', htmls) // html-ների թասք
-  gulp.watch("./*.html").on('change', browserSync.reload); // html թասք որը հետևում է բոլոր այն փոփոխություններին, որոնք կատարվում են բոլոր HTMLների մեջ
+	gulp.watch('./src/fonts/*', fonts) // fonts watch
+	gulp.watch('./src/libs/*', libs) // libs watch
+	gulp.watch('./src/js/**/*.js', scripts) // scripts watch
+	gulp.watch('./src/sass/**/*.scss', preproc) // sass watch 
+	gulp.watch('./src/css/**/*.css', styles) // css watch 
+	gulp.watch('./src/*.html', htmls)  // html watch
+	gulp.watch("./*.html").on('change', browserSync.reload); // html watch for all htmls
 }
 
-// gulp tasks, այստեղ բոլոր այն թասկերն են, որոնք մենք ստացել ենք վերևում, և փոխանցում ենք CMDին
+// all gulp tasks
 gulp.task('del', clean);
 gulp.task('htmls', htmls)
 gulp.task('sass', preproc);
@@ -136,12 +139,11 @@ gulp.task('imagemin', img)
 gulp.task('fonts', fonts);
 gulp.task('libs', libs);
 
-// watch task - սա watch-ը միացնելու տասկն է պարզապես CMDի մեջ գրում եք gulp watch և սեղմում enter
+// watch task for run live stream from build folder
 gulp.task('watch', watch);
 
-// build task - build-ը միացնելու տասկն է պարզապես CMDի մեջ գրում եք gulp build և սեղմում enter, և ձեր ֆայլերը build են լինում
-gulp.task('build', gulp.series(clean, gulp.parallel(htmls, preproc, styles, scripts, img, fonts, libs)));
+// build task for run build to all files from src to build folder
+gulp.task('all', gulp.series(clean, gulp.parallel(htmls, preproc, styles, scripts, img, fonts, libs)));
 
-// build => watch - սա սկզբում բիլդ է անում հետո արդեն watch, օգտակար է գլոբալ փոփոխությունների ժամանակ
-// կանչելու համար գրեք gulp dev
-gulp.task('dev', gulp.series('build','watch'));
+// dev task, parallel build and after watch
+gulp.task('dev', gulp.series('all','watch'));
